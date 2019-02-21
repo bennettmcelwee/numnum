fdfc7454f1e174d0d4b6212b59d62fcccba40408
Bennett McElwee <bennett@thunderguy.com>
2019-02-21 17:34:24 +1300

    Only show progress when there is something to show

--- src/Messager.js
+++ src/Messager.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default ({ messages }) => (
     <div className="messager">
-        <h2>Progress</h2>
         {messages && messages.length ?
-            messages.map((message, i) => <div key={i}>{message}</div>)
+            <>
+                <h2>Progress</h2>
+                { messages.map((message, i) => <div key={i}>{message}</div>) }
+            </>
             :
-            <p>Working...</p>
+            null
         }
     </div>
 );
